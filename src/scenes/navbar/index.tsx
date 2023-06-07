3fb73180ee3ba76910fb43b6839a8f1dcac4723d
Trushar Mandaviya <trusharmandaviya6@gmail.com>
2023-06-07 23:45:08 +0200

    added animation for closing of menu (for small devices only)

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -5,7 +5,7 @@ import Link from "./Link";
 import { SelectedPage } from "../../shared/types";
 import useMediaQuery from "../../hooks/useMediaQuery";
 import ActionButton from "../../shared/ActionButton";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 type Props = {
   isTopOfPage: boolean;
@@ -16,6 +16,7 @@ type Props = {
 const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const [isMenuClosing, setIsMenuClosing] = useState<boolean>(false);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
@@ -74,50 +75,66 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
       </div>
 
       {/* MOBILE MENU MODAL */}
-      {!isAboveMediumScreens && isMenuToggled && (
-        <motion.div
-          className="fixed bottom-0 right-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl"
-          initial="hidden"
-          animate={isMenuToggled ? "visible" : "hidden"}
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, x: 50 },
-            visible: { opacity: 1, x: 0 },
-          }}
-        >
-          {/* CLOSE ICON */}
-          <div className="flex justify-end p-12">
-            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
-              <XMarkIcon className="h-6 w-6 text-gray-400" />
-            </button>
-          </div>
+      <AnimatePresence>
+        {!isAboveMediumScreens && isMenuToggled && (
+          <motion.div
+            className="fixed bottom-0 right-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl"
+            initial="hidden"
+            animate={
+              isMenuToggled ? "visible" : isMenuClosing ? "closing" : "hidden"
+            }
+            exit="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: 50 },
+              visible: { opacity: 1, x: 0 },
+              closing: {
+                opacity: 0,
+                x: 50,
+                transition: { duration: 0.5 },
+              },
+            }}
+          >
+            {/* CLOSE ICON */}
+            <div className="flex justify-end p-12">
+              <button
+                onClick={() => {
+                  setIsMenuClosing(true);
+                  setTimeout(() => setIsMenuToggled(false), 500);
+                }}
+              >
+                <XMarkIcon className="h-6 w-6 text-gray-400" />
+              </button>
+            </div>
 
-          {/* MENU ITEMS */}
-          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
-            <Link
-              page="Home"
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
-            <Link
-              page="Benefits"
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
-            <Link
-              page="Our Classes"
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
-            <Link
-              page="Contact Us"
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
-          </div>
-        </motion.div>
-      )}
+            {/* MENU ITEMS */}
+            <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+              <Link
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Benefits"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Our Classes"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Contact Us"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </nav>
   );
 };
